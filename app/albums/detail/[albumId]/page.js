dfee8615bb2355b661d0a7f8ff0b228eeff593b2
ztarc12@gmail.com <ztarc12@gmail.com>
2025-03-10 14:02:52 +0900

    상세 페이지 디자인 수정 중

--- app/albums/detail/[albumId]/page.js
+++ app/albums/detail/[albumId]/page.js
@@ -30,62 +30,59 @@ export default function DetailArtists() {
   if (loading) return <p>불러오는 중...</p>;
   if (!data) return <p>데이터를 찾을수 없습니다.</p>;
   return (
-    <div className="main-cont detail">
+    <div className="main-cont detail pos-r">
+      <div className="detail-album-bg" style={{height: '30vh' ,backgroundImage: `url(${albumsDetail.images[0].url})`, backgroundSize: 'cover', backgroundPosition: 'center',}}></div>
       <h1>앨범</h1>
-      <div className="info-box">
-        <div className="image-flex">
-          <img src={albumsDetail.images[0].url}/>
-        </div>
-        <ul className="info-text">
-          <li>
-            <p>플레이리스트 제목</p>
-            <h3>{albumsDetail.name}</h3>
-          </li>
-          <li>
-            <p>아티스트 이름</p>
-            <h3>{albumsDetail.artists[0].name}</h3>
-          </li>
-          <li>
-            <p>곡 수</p>
-            <h3>{albumsDetail.total_tracks}</h3>
-          </li>
-          <li>
-            <p>외부링크</p>
-            <h3></h3>
-          </li>
-          <li>
-            <p>출시일</p>
-            <h3>{albumsDetail.release_date}</h3>
-          </li>
-        </ul>
-      </div>
-      <ul className="track-box">
-        {data.items.map((item)=>{
-          console.log('data.item',item)
-          return(
-            <li key={item.id} className="track">
-              <img src={albumsDetail.images?.[0].url} />
-              <h4 className="name-artist">
-                {item.name}
-                <span className="artist">
-                  {item.artists.map((artist) => artist.name).join(", ")}
-                </span>
-              </h4>
-              <div className="play-bar">
-                <button>
-                  <FontAwesomeIcon icon={faFolderPlus}/>
-                </button>
-                <button>
-                  <FontAwesomeIcon icon={faFileArrowDown}/>
-                </button>
-                <button>
-                  <FontAwesomeIcon icon={faPlay}/>
-                </button>
-              </div>
+        <div className="info-box">
+          <div className="image-flex">
+            <img src={albumsDetail.images[0].url}/>
+          </div>
+          <ul className="info-text">
+            <li>
+              <p>플레이리스트 제목</p>
+              <h3>{albumsDetail.name}</h3>
             </li>
-          )
-        })}
-      </ul>
+            <li>
+              <p>아티스트 이름</p>
+              <h3>{albumsDetail.artists[0].name}</h3>
+            </li>
+            <li>
+              <p>곡 수</p>
+              <h3>{albumsDetail.total_tracks}</h3>
+            </li>
+            <li>
+              <p>출시일</p>
+              <h3>{albumsDetail.release_date}</h3>
+            </li>
+          </ul>
+        </div>
+        <ul className="track-box">
+          {data.items.map((item)=>{
+            console.log('data.item',item)
+            return(
+              <li key={item.id} className="track">
+                <img src={albumsDetail.images?.[0].url} />
+                <h4 className="name-artist">
+                  {item.name}
+                  <span className="artist">
+                    {item.artists.map((artist) => artist.name).join(", ")}
+                  </span>
+                </h4>
+                <div className="play-bar">
+                  <button>
+                    <FontAwesomeIcon icon={faFolderPlus}/>
+                  </button>
+                  <button>
+                    <FontAwesomeIcon icon={faFileArrowDown}/>
+                  </button>
+                  <button>
+                    <FontAwesomeIcon icon={faPlay}/>
+                  </button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
     </div>
   );
 }
