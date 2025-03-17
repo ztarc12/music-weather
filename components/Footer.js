d1659d90d8d1b68b42f7d03ed349c9692343eae1
ztarc12@gmail.com <ztarc12@gmail.com>
2025-03-17 16:37:33 +0900

    플레이어 플레이리스트 / 반응형 진행중

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer(){
     return (
-        <div style={{ textAlign: 'center', color: '#bbb'}}>
+        <div style={{ textAlign: 'center', color: '#bbb', padding: '0.5rem'}}>
             © 2024. Lim Jae Ho. All rights reserved.
         </div>
     )
